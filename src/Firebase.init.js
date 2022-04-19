93e52bae6a7b894971c374e4a4d1970d7294282d
Nazmul Alam <shawonkst15@gmail.com>
2022-04-19 17:51:22 +0600

    add new env file

--- src/Firebase.init.js
+++ src/Firebase.init.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBknfgmjLrko3dy2QUWVv-ZfF8mpUmq7Ho",
-  authDomain: "wild-shot-go.firebaseapp.com",
-  projectId: "wild-shot-go",
-  storageBucket: "wild-shot-go.appspot.com",
-  messagingSenderId: "76721255956",
-  appId: "1:76721255956:web:c00d7f603af6543ad1d901"
+  apiKey:process.env.React_App_apiKey,
+  authDomain:process.env.React_App_authDomain,
+  projectId: process.env.React_App_projectId,
+  storageBucket:process.env.React_App_storageBucket,
+  messagingSenderId:process.env.React_App_messagingSenderId,
+  appId: process.env.React_App_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+export default auth;
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -9,13 +9,20 @@ const Register = () => {
     navigateReg('/login');
    
     }
+    const handleRegForm = event => {
+        event.preventDefault();
+         const name = event.target.name.value;
+         const email = event.target.email.value;
+         const pass = event.target.pasword.value;
+    }
     return (
         <div className='reg-form'>
             <h2>Please register</h2>
-             <form>
+             <form onSubmit={handleRegForm}>
                 <input type="text" name="name" id="" placeholder='Your Name' />
-                  <input type="email" name="name" id="" placeholder='Your email' />
+                  <input type="email" name="email" id="" placeholder='Your email' />
                   <input type="password" name="password" id="" placeholder='Your password'/>
+                  <input type="submit" value="Register"/>
              </form>
              <p> New user : <Link  to='/login' className='text-primary pe-auto text-decoration-none' onClick={navigateLogin}>Please Register</Link></p>
 
