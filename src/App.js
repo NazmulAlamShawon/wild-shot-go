7c2e0a8427256d34f21cf94bb60ebd1d642bc687
Nazmul Alam <shawonkst15@gmail.com>
2022-04-19 13:51:22 +0600

    add nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import About from './Pages/About/About';
+import Blog from './Pages/Blog/Blog';
+import Checkout from './Pages/Checkout/Checkout';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Services from './Pages/Services/Services';
+import Header from './Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
+          <Route path='/services' element={<Services></Services>}></Route>
+          <Route path='/checkout' element={<Checkout></Checkout>}></Route>
+          <Route path='/blog' element={<Blog></Blog>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+        </Routes>
     </div>
   );
 }
---
+++ src/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBknfgmjLrko3dy2QUWVv-ZfF8mpUmq7Ho",
+  authDomain: "wild-shot-go.firebaseapp.com",
+  projectId: "wild-shot-go",
+  storageBucket: "wild-shot-go.appspot.com",
+  messagingSenderId: "76721255956",
+  appId: "1:76721255956:web:c00d7f603af6543ad1d901"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
