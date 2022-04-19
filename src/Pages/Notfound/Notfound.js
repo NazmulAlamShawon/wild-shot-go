f9f2d54f8192f173fff812a4917997227863eca8
Nazmul Alam <shawonkst15@gmail.com>
2022-04-19 16:09:27 +0600

    add new form

--- src/Pages/Notfound/Notfound.js
+++ src/Pages/Notfound/Notfound.js
@@ -4,6 +4,7 @@ const Notfound = () => {
     return (
         <div>
            <h1>not found</h1> 
+           <p>404</p>
         </div>
     );
 };
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const Services = () => {
-    const {servicesId} = useParams();
+    const {servicesId,} = useParams();
     return (
         <div>
            <h2>Welcome to service details:{servicesId}</h2>
+           
         </div>
     );
 };
